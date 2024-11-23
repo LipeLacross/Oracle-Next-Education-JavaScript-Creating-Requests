e584e44af0336089e56dca59fbd6fb24c6b4d1d8
Felipe Moreira <87377686+LipeLacross@users.noreply.github.com>
2024-11-23 10:16:02 -0300

    update: render connect

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,12 +1,12 @@
 async function listaVideos() {
-    const conexao = await fetch("http://localhost:3000/videos");
+    const conexao = await fetch("https://oracle-next-education-javascript.onrender.com/videos");
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch("http://localhost:3000/videos", {
+    const conexao = await fetch("https://oracle-next-education-javascript.onrender.com/videos", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
@@ -27,7 +27,7 @@ async function criaVideo(titulo, descricao, url, imagem) {
 }
 
 async function buscaVideo(termoDeBusca) {
-    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexao = await fetch(`https://oracle-next-education-javascript.onrender.com/videos?q=${termoDeBusca}`);
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
